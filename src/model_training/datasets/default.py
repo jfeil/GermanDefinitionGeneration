54b34df4da393a158c242c98f24f07220a71717d
Jan Feil <github@jfeil.de>
2024-06-26 15:01:08 +0200

    Create default dataset for custom training loop

---
+++ src/model_training/datasets/default.py
@@ -0,0 +1,94 @@
+import re
+from typing import Tuple
+
+from datasets import Dataset
+
+from src.prompting import prompt_pattern
+from src.utils import sanitize_context, sanitize_context_word
+
+
+class DefaultDataset:
+    prompt_pattern = f"%s Was ist die Definition von %s? "
+    tokenizer = None
+
+    @staticmethod
+    def _subset(dataset, subset: float):
+        if subset < 1.0:
+            subset_val = int(len(dataset) * subset)
+        elif subset > 1.0:
+            subset_val = int(subset)
+        else:
+            raise ValueError(f'{subset} has to be larger or smaller than 1.0')
+
+        return dataset.select(range(subset_val))
+
+    @classmethod
+    def _data_loading(cls, shuffle: bool, seed: int, subset_train: float, subset_val: float) \
+            -> Tuple[Dataset, Dataset]:
+        # noinspection PyTypeChecker
+        dataset_train = Dataset.from_parquet("../dataset/v1/train.parquet", split="train")
+        # noinspection PyTypeChecker
+        dataset_val = Dataset.from_parquet("../dataset/v1/val.parquet", split="val")
+
+        if shuffle:
+            dataset_train = dataset_train.shuffle(seed=seed)
+            dataset_val = dataset_val.shuffle(seed=seed)
+
+        if subset_train > 0:
+            dataset_train = cls._subset(dataset_train, subset_train)
+        if subset_val > 0:
+            dataset_val = cls._subset(dataset_val, subset_val)
+
+        return dataset_train, dataset_val
+
+    @staticmethod
+    def _sanitize_spaces(input_text: str) -> str:
+        return re.sub(r'\s+', ' ', re.sub(r'\n+', ' ', input_text.strip()))
+
+    @classmethod
+    def _sanitize_context(cls, input_text: str) -> str:
+        input_text = input_text.replace("''", "")
+        input_text = cls._sanitize_spaces(input_text)
+        input_text = sanitize_context(input_text)
+        return input_text
+
+    @staticmethod
+    def _sanitize_word(input_text: str) -> str:
+        return sanitize_context_word(input_text)
+
+    @classmethod
+    def _preprocessing(cls, examples):
+        input_texts = [prompt_pattern(cls._sanitize_context(context), cls._sanitize_word(word),
+                                      pattern=cls.prompt_pattern) for
+                       context, word in zip(examples["context_sentence"], examples["context_word"])]
+        inputs = cls.tokenizer(input_texts, max_length=512, truncation=True)
+        inputs["labels"] = cls.tokenizer(text_target=[cls._sanitize_context(doc) for doc in examples["gt"]], max_length=128, truncation=True)["input_ids"]
+        inputs["debug_text"] = input_texts
+        inputs["debug_gt"] = [cls._sanitize_context(doc) for doc in examples["gt"]]
+
+        return inputs
+
+    @classmethod
+    def _prepare_data(cls, dataset_train: Dataset, dataset_val: Dataset) -> Tuple[Dataset, Dataset]:
+        if cls.tokenizer is None:
+            raise ValueError("Tokenizer must be set before preprocessing.")
+
+        dataset_train = dataset_train.map(cls._preprocessing, batched=True)
+        dataset_val = dataset_val.map(cls._preprocessing, batched=True)
+
+        return dataset_train, dataset_val
+
+    @classmethod
+    def create_dataset(cls, tokenizer, shuffle: bool, seed: int,
+                       subset_train: int | float = -1, subset_val: int | float = -1):
+        cls.tokenizer = tokenizer
+        dataset_train, dataset_val = cls._data_loading(shuffle, seed, subset_train, subset_val)
+
+        return cls._prepare_data(dataset_train, dataset_val)
+
+
+class DefinitionDataset(DefaultDataset):
+    """
+    Overwrite me :)
+    """
+    pass
--- src/training.py
+++ src/training.py
@@ -1,14 +1,76 @@
-from src.mongo_utils import get_all_entries, create_set
+import importlib
+import os
+import random
+import sys
+
+import click
+import numpy as np
+import torch
 
 
-def generator(split="train", limit=0):
-    pointer = get_all_entries(split=split, limit=limit)
-    buffer = []
+def set_seed(value):
+    random.seed(value)
+    np.random.seed(value)
+    torch.manual_seed(value)
+    torch.cuda.manual_seed_all(value)
 
-    for data in pointer:
-        if not buffer:
-            buffer += create_set(data, replace_none=True)
-        if len(buffer) == 0:
-            continue
-        title, context_word, context_example, gt = buffer.pop(0)
-        yield {'input': [title, context_word, context_example], 'gt': gt}
+
+def import_module_from_path(module_path):
+    # Ensure the path is absolute
+    module_path = os.path.abspath(module_path)
+
+    # Extract module name from path
+    module_name = os.path.splitext(os.path.basename(module_path))[0]
+
+    # Check if the module file exists
+    if not os.path.isfile(module_path):
+        raise FileNotFoundError(f"No such file: '{module_path}'")
+
+    # Load the module from the given path
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    if spec is None:
+        raise ImportError(f"Could not load spec from '{module_path}'")
+
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def get_all_class_variables(cls, prefix):
+    class_vars = {}
+    for base in cls.__mro__:
+        for key, value in base.__dict__.items():
+            # Filter out methods and special attributes
+            if type(value) is not classmethod and not callable(value) and not key.startswith('__'):
+                class_vars[prefix + "_" + key] = value
+    return class_vars
+
+
+@click.command()
+@click.option("--seed", type=int, default=42)
+@click.option("--shuffle", type=bool, default=True)
+@click.argument('model_path', type=click.Path(exists=True, file_okay=True, dir_okay=False),
+                default='model_training/training/default.py')
+@click.argument('dataset_path', type=click.Path(exists=True, file_okay=True, dir_okay=False),
+                default='model_training/datasets/default.py')
+@click.argument('experiment', type=int, default=3)
+@click.option("--subset-train", type=float, default=-1)
+@click.option("--subset-val", type=float, default=-1)
+def train(seed, shuffle, model_path, dataset_path, experiment, subset_train, subset_val):
+    """
+    Train LLMs
+
+    """
+    set_seed(seed)
+    dataset = import_module_from_path(dataset_path)
+    print(get_all_class_variables(dataset.DefinitionDataset, "dataset"))
+    return
+    dataset_train, dataset_val = dataset.DefinitionDataset.create_dataset(None, shuffle=shuffle, seed=seed,
+                                                                          subset_train=subset_train,
+                                                                          subset_val=subset_val)
+
+
+if __name__ == '__main__':
+    train()
