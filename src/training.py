575449f5235cdd6719540ef3d91e8170d46ef99a
Jan Feil <github@jfeil.de>
2024-06-27 20:40:32 +0200

    Move common functions into utils

--- src/training.py
+++ src/training.py
@@ -1,62 +1,16 @@
-import importlib
 import os
-import random
 import sys
 
 import click
-import numpy as np
-import torch
 from adapters import AdapterTrainer
 from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, EarlyStoppingCallback, Seq2SeqTrainer
+
+from src.utils import set_seed, import_module_from_path, get_all_class_variables
 
 sys.path.insert(0, '../')
 
 from src.ha_utils import HassioCallback
 from src.mlflow_utils import mlflow
-
-
-def set_seed(value):
-    random.seed(value)
-    np.random.seed(value)
-    torch.manual_seed(value)
-    torch.cuda.manual_seed_all(value)
-
-
-def import_module_from_path(module_path):
-    # Ensure the path is absolute
-    module_path = os.path.abspath(module_path)
-
-    # Extract module name from path
-    module_name = os.path.splitext(os.path.basename(module_path))[0]
-
-    # Check if the module file exists
-    if not os.path.isfile(module_path):
-        raise FileNotFoundError(f"No such file: '{module_path}'")
-
-    # Load the module from the given path
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
-    if spec is None:
-        raise ImportError(f"Could not load spec from '{module_path}'")
-
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-
-    return module
-
-
-def get_all_class_variables(cls, prefix):
-    class_vars = {}
-    for base in cls.__mro__:
-        for key, value in base.__dict__.items():
-            # Filter out methods and special attributes
-            name = prefix + "_" + key
-            if (name not in class_vars and
-                    type(value) is not classmethod and
-                    not callable(value) and
-                    not key.startswith('__')):
-                class_vars[name] = value
-    return class_vars
 
 
 @click.command()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,56 @@
 import re
 from typing import List, Tuple, Dict
+import importlib
+import os
+import random
+import sys
 
-from tqdm.auto import tqdm
+import numpy as np
+import torch
+
+
+def set_seed(value):
+    random.seed(value)
+    np.random.seed(value)
+    torch.manual_seed(value)
+    torch.cuda.manual_seed_all(value)
+
+
+def import_module_from_path(module_path):
+    # Ensure the path is absolute
+    module_path = os.path.abspath(module_path)
+
+    # Extract module name from path
+    module_name = os.path.splitext(os.path.basename(module_path))[0]
+
+    # Check if the module file exists
+    if not os.path.isfile(module_path):
+        raise FileNotFoundError(f"No such file: '{module_path}'")
+
+    # Load the module from the given path
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    if spec is None:
+        raise ImportError(f"Could not load spec from '{module_path}'")
+
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def get_all_class_variables(cls, prefix):
+    class_vars = {}
+    for base in cls.__mro__:
+        for key, value in base.__dict__.items():
+            # Filter out methods and special attributes
+            name = prefix + "_" + key
+            if (name not in class_vars and
+                    type(value) is not classmethod and
+                    not callable(value) and
+                    not key.startswith('__')):
+                class_vars[name] = value
+    return class_vars
 
 
 class GT(dict):
