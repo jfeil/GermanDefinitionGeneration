e0d62afb3d35e7ce6be38b8987bbc5a03a532021
Jan Feil <github@jfeil.de>
2024-09-03 15:08:23 +0200

    Add possibility to update calculated metrics with newly added ones

--- evaluation.py
+++ evaluation.py
@@ -5,13 +5,43 @@ import os
 import subprocess
 import tempfile
 from collections import defaultdict
+from logging import warning
 
 import click
 from typing import List, Dict, Callable
 
 import pandas
+from datasets import Dataset
 from rich.progress import track, Progress
 from tqdm.auto import tqdm
+
+
+def run_metrics(preds, metrics, batch_size, overwrite=False):
+    import torch
+    df = pandas.DataFrame.from_dict(preds)
+
+    for metric in tqdm(metrics, desc="Evaluating"):
+        if metric == "None":
+            continue
+        metric_results = defaultdict(list)  # type: Dict[str, List[float]]
+        cur_metric = standard_metrics[metric]()
+        for i in tqdm(range(0, len(df), batch_size), leave=False, desc=metric):
+            pred, target = preds['prediction'][i:i + batch_size], preds['gt'][i:i + batch_size]
+            result = cur_metric.calc_metric(pred, target)
+            for k, v in result.items():
+                metric_results[k] += v
+        del cur_metric
+        torch.cuda.empty_cache()
+        gc.collect()
+        for k, v in metric_results.items():
+            if overwrite and k in df.columns:
+                df = df.drop(k, axis=1)
+            elif not overwrite and k in df.columns:
+                warning(f"Metric {k} is already existing and overwriting is disabled. Ignoring.")
+            if k not in df.columns:
+                df.insert(len(df.columns), k, v, False)
+
+    return df
 
 
 def placeholder():
@@ -214,7 +244,7 @@ def load_model(run, device):
         # model needs to be reinstated
         with tempfile.TemporaryDirectory() as temp_path:
             weight_path = download_run_artifact(run, "model_data", temp_path)
-            model.from_pretrained(weight_path)
+            model = model.from_pretrained(weight_path)
         logging.info("Fine-tuned Model loaded.")
     else:
         raise ValueError("Invalid adapter information")
@@ -234,7 +264,7 @@ def load_model(run, device):
               type=click.Choice(list(standard_metrics.keys()), case_sensitive=False),
               default=standard_metrics.keys(),
               multiple=True)
-@click.option('--batch-size', type=int, default=32)
+@click.option('--batch-size', type=int, default=12)
 @click.option("--seed", type=int, default=42)
 @click.option("--shuffle", type=bool, default=True)
 @click.option("--subset-test", type=float, default=-1)
@@ -290,31 +320,16 @@ def evaluate(run, test_set, metrics, batch_size, seed, shuffle, subset_test, use
             yield item["prompt"]
 
     from tqdm.auto import tqdm
-    for out in tqdm(pipe(data_generator(), batch_size=batch_size, max_length=50, num_beams=5, early_stopping=True),
+    for out in tqdm(pipe(data_generator(), batch_size=batch_size*3, max_length=50, num_beams=5, early_stopping=True),
                     total=len(dataset_test), desc="Inferencing"):
         assert len(out) == 1
         test_predictions['prediction'].append(out[0]["generated_text"])
 
-    df = pandas.DataFrame.from_dict(test_predictions)
-
     del pipe, model, tokenizer
     torch.cuda.empty_cache()
 
-    for metric in tqdm(metrics, desc="Evaluating"):
-        if metric == "None":
-            continue
-        metric_results = defaultdict(list)  # type: Dict[str, List[float]]
-        cur_metric = standard_metrics[metric]()
-        for i in tqdm(range(0, len(df), batch_size), leave=False, desc=metric):
-            pred, target = test_predictions['prediction'][i:i + batch_size], test_predictions['gt'][i:i + batch_size]
-            result = cur_metric.calc_metric(pred, target)
-            for k, v in result.items():
-                metric_results[k] += v
-        del cur_metric
-        torch.cuda.empty_cache()
-        gc.collect()
-        for k, v in metric_results.items():
-            df.insert(len(df.columns), k, v, False)
+    df = run_metrics(test_predictions, metrics, batch_size)
+
     if output:
         df.to_json(output, orient="records", lines=True)
     else:
@@ -325,9 +340,57 @@ def evaluate(run, test_set, metrics, batch_size, seed, shuffle, subset_test, use
 
 @cli.command()
 @click.argument('test_set', type=click.Path(exists=True, file_okay=True, dir_okay=False),
+                default='/home/jfeil/MasterThesis/dataset_distillation/v0/test.parquet')
+@click.option('-m',
+              '--metrics',
+              type=click.Choice(list(standard_metrics.keys()), case_sensitive=False),
+              default=standard_metrics.keys(),
+              multiple=True)
+@click.option('--batch-size', type=int, default=32)
+@click.option("--seed", type=int, default=42)
+@click.option("--shuffle", type=bool, default=True)
+@click.option("--output", type=click.Path(exists=False, file_okay=True, dir_okay=False), default=None)
+@click.option('--debug', type=bool, default=False)
+@click.option("--experiment", type=int, default=0)
+@click.option("--dataset-fingerprint", type=str, default=None)
+def evaluate_parquet(test_set, metrics, batch_size, seed, shuffle, output, debug, experiment, dataset_fingerprint):
+    import torch
+    from src.mlflow_utils import mlflow
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    dataset_test = Dataset.from_parquet(test_set)
+    if shuffle:
+        dataset_test = dataset_test.shuffle(seed=seed)
+
+    test_predictions = {
+        'title': dataset_test['title'],
+        'context_sentence': dataset_test['context_sentence'],
+        'context_word': dataset_test['context_word'],
+        'gt': dataset_test['wiktionary_gt'],
+        'prediction': dataset_test['gt']
+    }
+
+    fingerprint = dataset_test._fingerprint if dataset_fingerprint is None else dataset_fingerprint
+
+    df = run_metrics(test_predictions, metrics, batch_size)
+    if output:
+        df.to_json(output, orient="records", lines=True)
+    else:
+        with mlflow.start_run(experiment_id=experiment):
+            mlflow.log_table(df, f"evaluation_{fingerprint}.json")
+            logging.info(f"data logged!")
+
+
+@cli.command()
+@click.argument('test_set', type=click.Path(exists=True, file_okay=True, dir_okay=False),
                 default='src/model_training/datasets/default_de.py')
 @click.option('-e', '--experiment', 'experiments', type=int, multiple=True, default=[])
 @click.option('-r', '--run', 'selected_runs', type=str, multiple=True, default=[])
+@click.option('--exclude', 'excluded_runs', type=str, multiple=True, default=[])
 @click.option('-m',
               '--metrics',
               type=click.Choice(list(standard_metrics.keys()), case_sensitive=False),
@@ -338,7 +401,7 @@ def evaluate(run, test_set, metrics, batch_size, seed, shuffle, subset_test, use
 @click.option("--shuffle", type=bool, default=True)
 @click.option("--subset-test", type=float, default=-1)
 @click.option('--debug', type=bool, default=False)
-def batch_evaluate(test_set, experiments, selected_runs, metrics, batch_size, seed, shuffle, subset_test, debug):
+def batch_evaluate(test_set, experiments, selected_runs, excluded_runs, metrics, batch_size, seed, shuffle, subset_test, debug):
     from src.mlflow_utils import get_run_list
 
     if debug:
@@ -347,7 +410,11 @@ def batch_evaluate(test_set, experiments, selected_runs, metrics, batch_size, se
         logging.basicConfig(level=logging.INFO)
 
     selected_runs += tuple(get_run_list(experiments))
-    selected_runs = tuple(set(selected_runs))
+    selected_runs = list(set(selected_runs))
+
+    for run in excluded_runs:
+        if run in selected_runs:
+            selected_runs.remove(run)
 
     logging.info("selected runs: %s", selected_runs)
     logging.info("selected metrics: %s", metrics)
@@ -388,7 +455,7 @@ def debug_metrics():
 @cli.command()
 @click.argument('run', type=str)
 @click.argument('test_set', type=click.Path(exists=True, file_okay=True, dir_okay=False),
-                default='src/model_training/datasets/default_de.py')
+                default='src/model_training/datasets/default.py')
 @click.option('--experiment', type=int)
 @click.option('--batch-size', type=int, default=32)
 @click.option("--seed", type=int, default=42)
@@ -425,31 +492,31 @@ def parameter_tuning(run, test_set, experiment, batch_size, seed, shuffle, subse
     strategies = [
         # {"name": "Greedy search"},
         # {"name": "Contrastive search", "penalty_alpha": 0.6, "top_k": 4, "max_new_tokens": 100},
-        {"name": "Multinomial search", "do_sample": True, "num_beams": 1, "max_new_tokens": 100},
-        {"name": "Beam-search decoding", "num_beams": 5, "max_new_tokens": 50},
-        {"name": "Beam-search multinomial sampling", "do_sample": True, "num_beams": 5},
-        {"name": "Diverse beam search decoding", "num_beams": 5, "num_beam_groups": 5, "max_new_tokens": 30,
-         "diversity_penalty": 1.0},
-        {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.1},
-        {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.2},
-        {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.3},
-        {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.4},
-        {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.5},
-        {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.1},
-        {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.2},
-        {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.3},
-        {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.4},
-        {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.5},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.01},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.1},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.3},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.5},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.7},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 0.9},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 1.1},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 1.2},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 1.4},
-        {"name": "Temperature", "num_beams": 5, "max_new_tokens": 50, "temperature": 1.5},
+        # {"name": "Multinomial search", "do_sample": True, "num_beams": 1, "max_new_tokens": 100},
+        # {"name": "Beam-search decoding", "num_beams": 5, "max_new_tokens": 50},
+        # {"name": "Beam-search multinomial sampling", "do_sample": True, "num_beams": 5},
+        # {"name": "Diverse beam search decoding", "num_beams": 5, "num_beam_groups": 5, "max_new_tokens": 30,
+        #  "diversity_penalty": 1.0},
+        # {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.1},
+        # {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.2},
+        # {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.3},
+        # {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.4},
+        # {"name": "Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "repetition_penalty": 1.5},
+        # {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.1},
+        # {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.2},
+        # {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.3},
+        # {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.4},
+        # {"name": "Encoder Repetition penalty", "num_beams": 5, "max_new_tokens": 50, "encoder_repetition_penalty": 1.5},
+        # {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.01},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.1},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.3},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.5},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.7},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 0.9},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 1.1},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 1.2},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 1.4},
+        {"name": "Temperature", "do_sample": True, "num_beams": 5, "max_new_tokens": 50, "temperature": 1.5},
     ]
 
     def data_generator():
@@ -467,19 +534,76 @@ def parameter_tuning(run, test_set, experiment, batch_size, seed, shuffle, subse
 
         logging.info("Currently used strategy: %s" % strategy)
         used_strategy = dict(strategy)
-        used_strategy.pop("name")
+        name = used_strategy.pop("name")
         for out in tqdm(pipe(data_generator(), batch_size=batch_size, **used_strategy),
                         total=len(dataset_test), desc="Inferencing"):
             assert len(out) == 1
             test_predictions['prediction'].append(out[0]["generated_text"])
         df = pandas.DataFrame.from_dict(test_predictions)
         with mlflow.start_run(experiment_id=experiment):
+            mlflow.set_tag('mlflow.runName', f"{name}")
             mlflow.log_params(strategy)
             mlflow.log_table(df, f"evaluation_{dataset_test._fingerprint}.json")
 
     del pipe, model, tokenizer
     torch.cuda.empty_cache()
 
+
+@cli.command()
+@click.option('-e', '--experiment', 'experiments', type=int, multiple=True, default=[])
+@click.option('-r', '--run', 'selected_runs', type=str, multiple=True, default=[])
+@click.option('--exclude', 'excluded_runs', type=str, multiple=True, default=[])
+@click.option('-m',
+              '--metrics',
+              type=click.Choice(list(standard_metrics.keys()), case_sensitive=False),
+              default=standard_metrics.keys(),
+              multiple=True)
+@click.option('--batch-size', type=int, default=12)
+@click.option('--overwrite', type=bool, default=False, is_flag=True)
+@click.option('--debug', type=bool, default=False)
+def update_metrics(experiments, selected_runs, excluded_runs, metrics, batch_size, overwrite, debug):
+    from src.mlflow_utils import get_run_list, mlflow, download_run_data
+    import re
+    import pandas as pd
+    from mlflow.store.artifact.artifact_repository_registry import get_artifact_repository
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    selected_runs += tuple(get_run_list(experiments))
+    selected_runs = list(set(selected_runs))
+
+    for run in excluded_runs:
+        if run in selected_runs:
+            selected_runs.remove(run)
+
+    logging.info("selected runs: %s", selected_runs)
+    logging.info("selected metrics: %s", metrics)
+
+    with Progress() as progress:
+        task = progress.add_task("Evaluating", total=len(selected_runs))
+        for run in selected_runs:
+            progress.update(task, description=f"Evaluating {run}", advance=0)
+
+            artifacts = mlflow.artifacts.list_artifacts(run_id=run)
+            for artifact in artifacts:
+                if artifact_name := re.match(r"evaluation_[0-9a-z]*.json", artifact.path):
+                    artifact_name = artifact_name.group(0)
+                    evaluation_data = download_run_data(run, artifact_name)
+                    if not evaluation_data:
+                        raise RuntimeError(f"Could not download evaluation data for run {run}")
+
+                    preds = pd.DataFrame(data=evaluation_data['data'], columns=evaluation_data['columns'])
+                    df = run_metrics(preds.to_dict(orient="list"), metrics, batch_size, overwrite)
+
+                    repository = get_artifact_repository(mlflow.get_run(run).info.artifact_uri)
+                    repository.delete_artifacts(artifact_name)
+                    with mlflow.start_run(run_id=run):
+                        mlflow.log_table(df, artifact_name)
+
+            progress.update(task, advance=1)
 
 if __name__ == '__main__':
     cli()
