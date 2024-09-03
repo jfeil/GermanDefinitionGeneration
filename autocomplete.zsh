65fc3ec52dca8957a7193524317d5f72042a4510
Jan Feil <github@jfeil.de>
2024-09-03 15:09:07 +0200

    Update autocomplete.zsh

--- autocomplete.zsh
+++ autocomplete.zsh
@@ -1,3 +1,4 @@
 autoload -Uz compinit && compinit
 eval "$(_TRAINING_COMPLETE=zsh_source training)"
 eval "$(_EVALUATE_COMPLETE=zsh_source evaluate)"
+eval "$(_ANALYSIS_COMPLETE=zsh_source analysis)"
